daf6fe77490c34784f395ca79bcc233fbc0de37a
MattiaCrovetto <92729247+MattiaCrovetto@users.noreply.github.com>
2021-11-15 00:11:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
+
 int main() {
- int n=1, a=0, b=1, c;
+ int n = 1, a = 0, b = 1, c;
    cin>>n;
-   if(n<=0){
-       cout << "  ";
+   if(n <= 0) {
+       cout << " ";
    } else {
        cout << " 1 " << endl;
-       for( int i=0; i<n;i++){
-
-           c= a+b;
-           a=b;
-           b=c;
-           if(c<=n){
+       for ( int i = 0; i<n ;i++){
+           c = a + b;
+           a = b;
+           b = c;
+           if (c<=n){
                cout << c << endl;
            } else {
                break;
